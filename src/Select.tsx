1bfd57dfb46f6198199d1ba2f94e42ddca1d446c
Victoria <viktoriya.zapisochnaya@gmail.com>
2023-10-05 23:19:23 +0300

    toGetChanges

--- src/Select.tsx
+++ src/Select.tsx
@@ -1,28 +1,46 @@
 import React from "react";
-import styles from './select.module.css'
+import styles from "./select.module.css";
 
 type SelectOption = {
   label: string;
   value: any;
 };
 
-type SelectProps = {
+type MultipleSelectProps = {
+  multiple: true;
+  value?: SelectOption[];
+  onChange: (value: SelectOption[] ) => void;
+}
+
+type SingleSelectProps = {
+  multiple?: false;
   value?: SelectOption;
   onChange: (value: SelectOption | undefined) => void;
-  options: SelectOption[];
-};
+}
 
-export default function Select({ value, onChange, options }: SelectProps) {
+type SelectProps = {
+  options: SelectOption[];
+} & (SingleSelectProps | MultipleSelectProps)
+
+export default function Select({
+  multiple,
+  value,
+  onChange,
+  options,
+}: SelectProps) {
   return (
-  <div tabIndex={0} className={styles.container}>
-    <span className={styles.value}>Value</span>
-    <button className={styles['clear-btn']}>x</button>
-    <div className={styles.divider}></div>
-    <div className={styles.caret}></div>
-    <ul className={styles.options}>
-      {options.map(el => (
-        <li className={styles.option} key={el.label}>{el.label}</li>
-      ) )}
-    </ul>
-  </div>)
+    <div tabIndex={0} className={styles.container}>
+      <span className={styles.value}>Value</span>
+      <button className={styles["clear-btn"]}>x</button>
+      <div className={styles.divider}></div>
+      <div className={styles.caret}></div>
+      <ul className={styles.options}>
+        {options.map((el) => (
+          <li className={styles.option} key={el.label}>
+            {el.label}
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
 }
